56d6f841d4207b2af0106897569f534433959056
FOXYCRON <57571091+FOXYCRON@users.noreply.github.com>
2022-06-24 12:19:43 -0600

    Add files via upload

---
+++ ciclofor.php
@@ -0,0 +1,19 @@
+<html> 
+  <head>
+     <title> Ciclo For </title>
+    </head>
+    <body>
+    <?php
+
+
+    for ($x=500; $x>=300; $x=$x-5)
+     {
+       echo "En esta vuelta X tiene el valor de: ";
+       echo $x;
+       echo "<br>";
+      }
+
+?>
+
+</body>
+</html>
---
+++ ciclofor2.php
@@ -0,0 +1,19 @@
+<html> 
+  <head>
+     <title> Ciclo For 2 </title>
+    </head>
+    <body>
+    <?php
+
+
+    for ($x=100; $x<=2000; $x=$x+10)
+     {
+       echo "En esta vuelta X tiene el valor de: ";
+       echo $x;
+       echo "<br>";
+      }
+
+?>
+
+</body>
+</html>
